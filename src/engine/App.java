ae1ec2d3a37fcbb4f6fb9aa10f02861f53e9e32d
Ptitmonstre <aubert.gwendal@gmail.com>
2015-12-30 21:57:03 +0100

    Server MAJ w/ request

--- src/engine/App.java
+++ src/engine/App.java
@@ -1,32 +1,42 @@
 package engine;
 
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import rest.Client;
  
 public class App {
-    public static void main(String[] args) throws Exception {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
- 
-        Server jettyServer = new Server(8080);
-        jettyServer.setHandler(context);
- 
-        ServletHolder jerseyServlet = context.addServlet(
-             org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(0);
- 
-        // Tells the Jersey Servlet which REST service/class to load.
-        jerseyServlet.setInitParameter(
-           "jersey.config.server.provider.classnames",
-           EntryPoint.class.getCanonicalName());
- 
-        try {
-            jettyServer.start();
-            jettyServer.join();
-        } finally {
-            jettyServer.destroy();
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+        ServletHandler handler = new ServletHandler();
+        handler.addServletWithMapping(HelloServlet.class, "/hello");//Set the servlet to run.
+        handler.addServletWithMapping(Client.class, "/search");//Set the servlet to run.
+        server.setHandler(handler);    
+        server.start();
+        server.join();
+    }
+
+    @SuppressWarnings("serial")
+    public static class HelloServlet extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println("<h1>Hello SimpleServlet</h1>");
         }
     }
+
 }
 
---
+++ src/rest/Client.java
@@ -0,0 +1,50 @@
+package rest;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@SuppressWarnings("serial")
+public class Client extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String req = request.getQueryString();
+    	String[] splitted = req.split("&|=");
+    	
+    	//Si parametre query
+    	if(splitted.length>0 && (splitted[0].equals("query"))){
+    		
+    		//Si parametre flag
+    		if(splitted.length > 2 && (splitted[2].equals("flag"))){
+    			this.query(splitted[1], splitted[3], response);
+    		}
+    		//Si autre parametre que flag, on ne s'en charge pas
+    		else{
+    			this.query(splitted[1], "", response);
+    		}
+    	}
+    	else{
+    		response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println("<p>"+splitted[0]+"</p>");
+            response.getWriter().println("<p>"+splitted[1]+"</p>");
+            response.getWriter().println("<p>"+splitted[2]+"</p>");
+            response.getWriter().println("<p>"+splitted[3]+"</p>");
+            response.getWriter().println("<p>Usage : /search?query=<your_query> or /search?query=<your_query>&flag=<flag></p>");
+    	}
+
+    }
+    
+    //Lance la query et traite une reponse
+    public void query(String q, String flag, HttpServletResponse response) throws ServletException, IOException{
+    	response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<p>Requete : "+q+"</p>");
+        response.getWriter().println("<p>Categorie : "+flag+"</p>");
+    }
+    
+}
