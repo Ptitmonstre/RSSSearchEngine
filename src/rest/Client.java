34da7ea92f6c7ade76b79b84bf91bf95cf4ad8ef
Ptitmonstre <aubert.gwendal@gmail.com>
2015-12-31 11:34:46 +0100

    Client request working

--- src/rest/Client.java
+++ src/rest/Client.java
@@ -1,15 +1,36 @@
 package rest;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexNotFoundException;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
  
 @SuppressWarnings("serial")
 public class Client extends HttpServlet {
 
+	private static String indexFolder="./data/indexes";
+	private Directory index;
+	private StandardAnalyzer analyzer = new StandardAnalyzer();
+	private IndexSearcher searcher;
+	
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	String req = request.getQueryString();
@@ -42,11 +63,69 @@ public class Client extends HttpServlet {
 
     }
     
+    public void init(){
+    	Path path = Paths.get(indexFolder);
+		try {
+			index = FSDirectory.open(path);
+	    }catch (IndexNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}	
+    }
+    
     //Lance la query et traite une reponse
-    public void query(String q, String flag, HttpServletResponse response) throws ServletException, IOException{
+    public void query(String query, String flag, HttpServletResponse response) throws ServletException, IOException{
+    	
+		try {
+			//lancer la classification
+			IndexReader reader = DirectoryReader.open(index);
+			searcher= new IndexSearcher(reader);
+			Query q = null;
+			if(query.equals("language:en") || query.equals("language:fr")){
+				String language = query.split(":")[1];
+				
+				QueryParser qp=new QueryParser("", analyzer);
+				if(flag == "")
+					q =qp.parse("language:\""+language+"\"");
+				else
+					q =qp.parse("language:\""+language+"\" AND category:\""+flag+"\"");	
+			}
+			else{
+				q =MultiFieldQueryParser.parse(new String[]{query,query,query,query,query} , new String[] {"title","author","content","description","language"}, analyzer);
+			}
+			
+			double nbRes=searcher.count(q);
+			
+			//ajoute la cat�gorie dans le vecteur des cat�gories possibles
+			if(nbRes>0){
+				
+				TopDocs docs=searcher.search(q, 1000);
+				Document document;
+
+				for(ScoreDoc hit : docs.scoreDocs){//pour tout �l�ment d'apprentissage
+					//stemming
+					document=searcher.doc(hit.doc);
+					//document.get("content")
+					//document.get("language")
+					response.setContentType("text/html");
+			        response.setStatus(HttpServletResponse.SC_OK);
+			        
+			        response.getWriter().println("<div style='width: 80%; margin: 0 auto; margin-bottom: 30px;'>");
+			        response.getWriter().println("<p><a href='"+document.get("url_src")+"'>"+document.get("title")+"</a></p>");
+			        response.getWriter().println("<span style='margin-right: 30px;'> Auteur : "+document.get("author")+"</span>"+"<span> Date : "+document.get("date")+"</span>");
+			        response.getWriter().println("<p>"+document.get("content")+"</p>");
+			        response.getWriter().println("</div>");
+				}
+			}
+			
+		}catch (IOException | org.apache.lucene.queryparser.classic.ParseException e) {
+			System.err.println("Couldn't read the index");
+		}
+
     	response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<p>Requete : "+q+"</p>");
+        response.getWriter().println("<p>Requete : "+query+"</p>");
         response.getWriter().println("<p>Categorie : "+flag+"</p>");
         
         //TODO exemple : http://stackoverflow.com/a/5100727
